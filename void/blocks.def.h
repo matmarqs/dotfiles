de2cee7c797f96663551a0d65d8073d6e37406f2
matmarqs <84104312+matmarqs@users.noreply.github.com>
2021-10-14 15:25:32 -0300

    Add files via upload

---
+++ void/blocks.def.h
@@ -0,0 +1,14 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+  /*Icon*/    /*Command*/	                              /*Update Interval*/	    /*Update Signal*/
+  {"",        "/home/sekai/scripts/statusbar-bat",	    60,		                  11},
+  {"",        "/home/sekai/scripts/statusbar-vol",      30,                     10},
+  {"",        "/home/sekai/scripts/statusbar-hor",	    60,		                  12},
+};
+
+// Comando alternativo para obter o volume:
+// amixer get Master | grep "Left:" | awk -F'[][]' '{print $2}'
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
